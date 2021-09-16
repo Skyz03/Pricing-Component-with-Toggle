a0dcd12f4fc299074eecb362ae3a49d1eb2f4a28
Syz03 <aakibdiscoo@gmail.com>
2021-09-16 22:31:01 +0545

    Improvements in the JS

--- js/index.js
+++ js/index.js
@@ -1,17 +1,25 @@
-const SWITCH_CHECKBOX = document.querySelector("#switch");
+let SWITCH_CHECKBOX = document.querySelector("#switch");
 
 //creation of the loop for selecting the price elements
 const noOfElements = document.querySelectorAll(".price").length
-for (let i = 0; i < noOfElements; i++) {
 
-  let priceElements = document.querySelectorAll(".price")[i];
+function priceChange() {
 
+  for (let i = 0; i < noOfElements; i++) {
 
-  if (SWITCH_CHECKBOX.checked) {
-    priceElements = priceElements.innerHTML / 10;
+    let priceElements = document.querySelectorAll(".price")[i];
+    let element = priceElements.textContent;
+    console.log(element);
+    let numberElements = (parseFloat(element)).toFixed(2);
+    // console.log(numberElements);
+
+    if (SWITCH_CHECKBOX.checked === true) {
+
+      priceElements.innerHTML = numberElements / 10;
+
+    }
+
+    priceElements = priceElements.innerHTML;
 
   }
-
-  priceElements = priceElements.innerHTML;
-
 }
