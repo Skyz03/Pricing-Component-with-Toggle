c49e31febc914e624223c8d6c15f71999718cd33
Syz03 <aakibdiscoo@gmail.com>
2021-09-17 21:57:13 +0545

    Updates in the JS code Working

--- js/index.js
+++ js/index.js
@@ -7,19 +7,22 @@ function priceChange() {
 
   for (let i = 0; i < noOfElements; i++) {
 
-    let priceElements = document.querySelectorAll(".price")[i];
-    let element = priceElements.textContent;
-    console.log(element);
-    let numberElements = (parseFloat(element)).toFixed(2);
+    let priceElements = document.querySelectorAll(".price");
+
+    // let element = priceElements.textContent;
+    // console.log(element);
+    // let numberElements = (parseFloat(element)).toFixed(2);
     // console.log(numberElements);
 
     if (SWITCH_CHECKBOX.checked === true) {
 
-      priceElements.innerHTML = numberElements / 10;
-
+      priceElements[0].innerHTML = "$19.99";
+      priceElements[1].innerHTML = "$24.99";
+      priceElements[2].innerHTML = "$39.99";
+    } else {
+      priceElements[0].innerHTML = "$199.99";
+      priceElements[1].innerHTML = "$249.99";
+      priceElements[2].innerHTML = "$399.99";
     }
-
-    priceElements = priceElements.innerHTML;
-
   }
 }
